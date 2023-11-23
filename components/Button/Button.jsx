aa29c35762ba36ea58442df9c92ae61853355f7d
Kevin <cartek279@gmail.com>
2023-11-23 23:47:24 +0200

    Refactor App component and add Home page

---
+++ components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css'
+
+function Button() {
+  return (
+    <div className='styles'>Button</div>
+  )
+}
+
+export default Button
---
+++ page/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+
+const Home = () => {
+  return (
+    <div className="main-body">
+        <h1>Task schedure</h1>
+    </div>
+  )
+}
+
+export default Home
