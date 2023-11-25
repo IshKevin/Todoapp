ad353b7c6f8f7e1c75658a8bbbea0cb43142b054
Kevin <cartek279@gmail.com>
2023-11-26 00:51:53 +0200

    Add AddTodo component with CSS styling

---
+++ page/AddTodo/AddTodo.jsx
@@ -0,0 +1,41 @@
+import { useState, useContext, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { TodoContext } from '../../context/TodoContext'
+import './AddTodo.css'
+
+const AddTodo = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useContext(TodoContext);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    const newTodo = [...todos, { id: uuidv4(), text: inputValue, completed: false }];
+    setTodos(newTodo);
+    setInputValue('');
+  };
+  
+  useEffect(() => {
+    console.log('Storing todos in local storage:', todos);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <>
+      <div>Task schedule</div> 
+      <div className="AddTodo">
+        <input 
+          type="textbox" 
+          value={inputValue} 
+          onChange={handleInputChange}  
+          className="input-box"/> 
+        <button type="button" onClick={handleAddTodo}>Add</button>   
+      </div>
+    </>
+  )
+}
+
+export default AddTodo
