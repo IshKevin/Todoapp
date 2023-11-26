e41a596af863bca7b49ede06b0e5541d945b36cf
Kevin <cartek279@gmail.com>
2023-11-26 02:57:08 +0200

    Refactor Home component and add navigation bar

--- page/Home/Home.jsx
+++ page/Home/Home.jsx
@@ -1,16 +1,30 @@
-import AddTodo from '../AddTodo/AddTodo'
-import TodoList from '../TodoList/TodoList'
-import { Todos } from '../Todos/Todos'
-import './Home.css'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import AddTodo from '../AddTodo/AddTodo';
 
+
+import './Home.css';
+import Slider from '../Slider/Slider';
 
 const Home = () => {
   return (
-    <div className="main-body">
-      <AddTodo/>
-      <TodoList/>
-    </div>
-  )
-}
+    <Router>
+      <div className="main-body">
+        <AddTodo />
+        <nav>
+          <ul className='navBar'>
+            <li><Link to="/important">Important </Link></li>
+            
+            <li><Link to="/completed">Completed </Link></li>
+            <li><Link to="/all">All Tasks</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/*" element={<Slider />} />
+          <Route path="/" element={<Navigate to="/all" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default Home
+export default Home;
