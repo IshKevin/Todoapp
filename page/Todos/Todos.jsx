0329c44ba3e57513776a4708d71a88dc71643490
Kevin <cartek279@gmail.com>
2023-11-26 06:02:31 +0200

    Update Todos component styles

--- page/Todos/Todos.jsx
+++ page/Todos/Todos.jsx
@@ -3,17 +3,14 @@ import './Todos.css'
 import { TodoContext } from '../../context/TodoContext'
 import { FaTrash } from 'react-icons/fa';
 import { MdLabelImportant } from "react-icons/md";
+import { MdDone } from "react-icons/md";
+import { IoMdCreate } from "react-icons/io";
 
 export const Todos = ({ id, text,completed }) => {
   const [todos, setTodos] = useContext(TodoContext);
   const completedTodo = (e) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex((todo) => todo.id === e.target.value);
-    newTodos[todoIndex] = {
-      ...newTodos[todoIndex],
-      completed: !newTodos[todoIndex].completed,
-    };
-    setTodos(newTodos);
+    const id = e.target.value;
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: true } : todo));
   }
 
   const deletTodo = (e) => {
@@ -22,20 +19,20 @@ export const Todos = ({ id, text,completed }) => {
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
   }
-  const isChecked = completed ? 'checked' : '';
   return (
     <div className='todoList'>
-      <input type="checkbox" 
+      {/* <input type="checkbox" 
       className="checkbox" 
       id={id} 
       value={id}
       checked={isChecked}
-      onChange={e=> completedTodo(e)}/>
-      <div className="todoItem">{text}</div> {/* Update to use text instead of title */}
-      <div>
-      <button type="button" className="delete" id={id}
-      onClick={e=>deletTodo(e)}><FaTrash/></button>
-        <button type="button" className="important"><MdLabelImportant/></button>
+      onChange={e=> completedTodo(e)}/> */}
+      <div className={todos.completed ? 'completed' : 'todoItem'}>{text}</div> {/* Update to use text instead of title */}
+      <div className='buttons'>
+       <button type="button" className="completed" value={id}onClick={e=>completedTodo(e)}><MdDone/></button>
+       <button type="button" className="important"><MdLabelImportant/></button>
+       <button type="button" className="edit"><IoMdCreate /></button>
+       <button type="button" className="delete" id={id}onClick={e=>deletTodo(e)}><FaTrash/></button> 
         </div>
     </div>
   )
